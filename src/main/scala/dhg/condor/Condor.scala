8218f2ab14160cd3ff7d78b1afd6dd16004b4fd4
Dan Garrette <dhgarrette@gmail.com>
2013-12-24 12:43:28 -0600

    more functionality

--- src/main/scala/dhg/condor/Condor.scala
+++ src/main/scala/dhg/condor/Condor.scala
@@ -2,13 +2,32 @@ package dhg.condor
 
 import dhg.util.CollectionUtil._
 import dhg.util.FileUtil._
+import dhg.util.StringUtil._
 import dhg.util.Pattern._
 
+/**
+ * target/start dhg.condor.CondorFromFile runsfile
+ * target/start dhg.condor.CondorFromFile rerun runsfile name
+ */
 class Condor(
   stagingDir: String,
   memory: Int = 4000) {
 
-  def make(classnamesArgsAndFilenames: Iterable[(String, String, String)]) = {
+  def makeNamed(classnamesAndArgs: Vector[(String, String)]): Unit = {
+    val digits = math.log10(classnamesAndArgs.size).toInt + 1
+    makeWithNames(classnamesAndArgs.mapt {
+      case (c, a) => (c, a, (c +: a.split("\\s+")).mkString("_"))
+    })
+  }
+
+  def makeNumbered(classnamesAndArgs: Vector[(String, String)]): Unit = {
+    val digits = math.log10(classnamesAndArgs.size).toInt + 1
+    makeWithNames(classnamesAndArgs.zipWithIndex.map {
+      case ((c, a), i) => (c, a, i.toString.padLeft(digits, "0"))
+    })
+  }
+
+  def makeWithNames(classnamesArgsAndFilenames: Vector[(String, String, String)]): Unit = {
     val workingDir = File("").getAbsolutePath
 
     writeUsing(File(stagingDir, "main.config")) { w =>
@@ -52,3 +71,22 @@ class Condor(
 
 }
 
+object CondorFromFile {
+  def main(args: Array[String]): Unit = {
+    args.head match {
+      case "rerun" =>
+        val Seq(_, filename, name) = args.toList
+        val Seq(classname, argstring) = name.lsplit("_", 2)
+        new Condor(pathjoin("condorfiles", filename.split("\\.").head))
+          .makeNamed(Vector((classname, argstring.replace("_", " "))))
+      case _ =>
+        val Seq(filename) = args.toList
+        val lines = File(filename).readLines.toVector
+        new Condor(pathjoin("condorfiles", filename.split("\\.").head))
+          .makeNamed(lines.filter(_.nonEmpty).map { line =>
+            val Seq(_, classname, argstring) = line.lsplit("\\s+", 3)
+            (classname, argstring)
+          })
+    }
+  }
+}
